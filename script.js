98710a3f08bc2d8c6b5003386237d5cda1d273e3
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-12-02 01:25:15 -0800

    add pen trail  to screen

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let log = console.log;
 
 const gridContainer = document.querySelector("#gridContainer");
-log(gridContainer);
+
+function pixelateDiv(e) {
+    log(e.target);
+    e.target.style.background="#666"
+  }
 
 for (let row = 0; row < 16; row++) {
   let rowContainer = document.createElement("div");
@@ -9,10 +13,14 @@ for (let row = 0; row < 16; row++) {
   rowContainer.style.height = "calc(100vh / 16)";
   for (let col = 0; col < 16; col++) {
     let squareDiv = document.createElement("div");
-    squareDiv.textContent = `${row},${col}`;
     squareDiv.className = "squareDiv";
     squareDiv.style.width = "calc(100vw/16)";
     rowContainer.appendChild(squareDiv);
   }
   gridContainer.appendChild(rowContainer);
 }
+
+allSquareDivs = document.querySelectorAll(".squareDiv");
+allSquareDivs.forEach((squareDiv) =>
+  squareDiv.addEventListener("mouseenter", pixelateDiv)
+);
