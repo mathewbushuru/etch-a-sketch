97bb17d53281b0c0f6d3d555a2d9d15942cbefc1
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-12-02 00:34:49 -0800

    set height and width  with js so that it can be modified  dynamically

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ log(gridContainer);
 for (let row = 0; row < 16; row++) {
   let rowContainer = document.createElement("div");
   rowContainer.className = "rowContainer";
+  rowContainer.style.height = "calc(100vh / 16)";
   for (let col = 0; col < 16; col++) {
     let squareDiv = document.createElement("div");
     squareDiv.textContent = `${row},${col}`;
     squareDiv.className = "squareDiv";
+    squareDiv.style.width = "calc(100vw/16)";
     rowContainer.appendChild(squareDiv);
   }
   gridContainer.appendChild(rowContainer);
