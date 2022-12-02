0b21059142c8541eb73ff20f8e63e4acc9524872
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-12-02 02:14:48 -0800

    user can modify num of squares in sketchpad

--- script.js
+++ script.js
@@ -2,25 +2,39 @@ let log = console.log;
 
 const gridContainer = document.querySelector("#gridContainer");
 
-function pixelateDiv(e) {
-    log(e.target);
-    e.target.style.background="#666"
-  }
+let numSquares = 16;
+const numSquaresBtn = document.querySelector("#numSquaresBtn");
+numSquaresBtn.addEventListener("click", () => {
+  numSquares = prompt("How many squares per side? (max is 100 squares) ", 32);
+  if (numSquares > 100) numSquares = 100;
+  gridContainer.innerHTML = "";
+  drawGrid(numSquares);
+});
 
-for (let row = 0; row < 16; row++) {
-  let rowContainer = document.createElement("div");
-  rowContainer.className = "rowContainer";
-  rowContainer.style.height = "calc(100vh / 16)";
-  for (let col = 0; col < 16; col++) {
-    let squareDiv = document.createElement("div");
-    squareDiv.className = "squareDiv";
-    squareDiv.style.width = "calc(100vw/16)";
-    rowContainer.appendChild(squareDiv);
-  }
-  gridContainer.appendChild(rowContainer);
+function pixelateDiv(e) {
+  e.target.style.background = "#666";
 }
 
-allSquareDivs = document.querySelectorAll(".squareDiv");
-allSquareDivs.forEach((squareDiv) =>
-  squareDiv.addEventListener("mouseenter", pixelateDiv)
-);
+function drawGrid(numSquares) {
+  for (let row = 0; row < numSquares; row++) {
+    let rowContainer = document.createElement("div");
+    rowContainer.className = "rowContainer";
+    rowContainer.style.height = `calc(86vh/${numSquares})`;
+    for (let col = 0; col < numSquares; col++) {
+      let squareDiv = document.createElement("div");
+      squareDiv.className = "squareDiv";
+      squareDiv.style.width = `calc(95vw/${numSquares})`;
+      squareDiv.style.border = `${32 / numSquares}px solid #dedede`;
+      rowContainer.appendChild(squareDiv);
+      squareDiv.addEventListener("mouseenter", pixelateDiv);
+    }
+    gridContainer.appendChild(rowContainer);
+  }
+}
+
+drawGrid(numSquares);
+
+// allSquareDivs = document.querySelectorAll(".squareDiv");
+// allSquareDivs.forEach((squareDiv) =>
+//   squareDiv.addEventListener("mouseenter", pixelateDiv)
+// );
